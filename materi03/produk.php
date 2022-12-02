c391ed74c84b464fb2d7308e3d7821e42d337491
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-03 03:13:08 +0700

    pembuatan object yang diambil dari class produk

--- materi03/produk.php
+++ materi03/produk.php
@@ -6,11 +6,15 @@ class Produk{
             $penulis = "penulis",
             $penerbit = "penerbit",
             $harga = 0;
-            
+
 
 }
 
 
 
-
+$produk3 = new Produk(),
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
 ?>
