eb02cdf06787c3d8038c7467a3b9ad00ae7f270b
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-03 03:17:06 +0700

    menambahkan object produk4

--- materi03/produk.php
+++ materi03/produk.php
@@ -19,4 +19,15 @@ $produk3->judul = "Naruto";
 $produk3->penulis = "Masashi Kishimoto";
 $produk3->penerbit = "Shonen Jump";
 $produk3->harga = 30000;
+
+$produk4 = new Produk(),
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil D";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+
+
+
+
 ?>
