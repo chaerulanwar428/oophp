7f72a3c3ad9835ffdb2d618d64de7b9527c1c531
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-03 03:20:20 +0700

    memperbaikki bug

--- materi03/produk.php
+++ materi03/produk.php
@@ -14,13 +14,13 @@ class Produk{
 
 
 
-$produk3 = new Produk(),
+$produk3 = new Produk();
 $produk3->judul = "Naruto";
 $produk3->penulis = "Masashi Kishimoto";
 $produk3->penerbit = "Shonen Jump";
 $produk3->harga = 30000;
 
-$produk4 = new Produk(),
+$produk4 = new Produk();
 $produk4->judul = "Uncharted";
 $produk4->penulis = "Neil D";
 $produk4->penerbit = "Sony Computer";
