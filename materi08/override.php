072ef3fc8031e7e91922c5612a008a94c18ca81b
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-04 22:21:40 +0700

    melakukan overrride pada class komik

--- materi08/override.php
+++ materi08/override.php
@@ -33,6 +33,13 @@ class Produk{
 
 
 class Komik extends Produk{
+    public $jmlHalaman;
+
+    public function __construct($judul ="judul", $penulis = "penulis", $penerbit ="penerbit" , $harga = 0, $waktuMain = 0){
+        parent::__construct($judul,$penulis,$penerbit,$harga);
+
+        $this->waktuMain = $waktuMain;
+    }
     public function getInfoProduk(){
         $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
         return $str;
