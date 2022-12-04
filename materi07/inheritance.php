63519c5ffede4692e82bcf30696b4673810d24dd
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-04 20:21:29 +0700

    menjalankan class komik dan game

--- materi07/inheritance.php
+++ materi07/inheritance.php
@@ -7,17 +7,15 @@ class Produk{
             $penerbit,
             $harga,
             $jmlHalaman,
-            $waktuMain,
-            $tipe;
+            $waktuMain;
 
-    public function __construct($judul ="judul", $penulis = "penulis",$penerbit = "penerbit",$harga = 0, $jmlHalaman= 0 ,$waktuMain = 0, $tipe){
+    public function __construct($judul ="judul", $penulis = "penulis",$penerbit = "penerbit",$harga = 0, $jmlHalaman= 0 ,$waktuMain = 0){
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
         $this->jmlHalaman = $jmlHalaman;
         $this->waktuMain = $waktuMain;
-        $this->tipe = $tipe;
     }
     
 
@@ -28,12 +26,7 @@ class Produk{
 
     public function getInfoProduk(){
         //komik: Naruto | mashasi kishimoto, shonen jump (Rp. 30000) - 100 Halaman.
-        $str = "{$this->tipe} : {$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
-        if ($this-> tipe == "Komik"){
-            $str .= " - {$this -> jmlHalaman} Halaman.";
-        } else if ($this->tipe == "Game"){
-            $str .= " ~ {$this -> waktuMain} Jam.";
-        }
+        $str = "{$this->judul} | {$this->getLabel()} (Rp.{$this->harga})";
         return $str;
     }
 }
@@ -42,9 +35,14 @@ class Produk{
 class Komik extends Produk{
     public function getInfoProduk(){
         $str = "Komik : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->jmlHalaman} Halaman.";
+        return $str;
     }
-
-
+}
+class Game extends Produk{
+    public function getInfoProduk(){
+        $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
+        return $str;
+    }
 }
 
 class CetakInfoProduk{
@@ -55,14 +53,14 @@ class CetakInfoProduk{
 }
 
 
-$produk1 = new Produk("Naruto","Masashi Kishimoto", "Shonen Jump", 30000,100,0,"Komik");
+$produk1 = new Komik("Naruto","Masashi Kishimoto", "Shonen Jump", 30000,100,0);
 
-$produk2 = new Produk("Uncharted", "Neil D", "Sony Computer", 250000,0,50,"Game");
+$produk2 = new Game("Uncharted", "Neil D", "Sony Computer", 250000,0,50);
 
 
-echo $produk1 -> getInfoLengkap();
+echo $produk1 -> getInfoProduk();
 echo"<br>";
-echo $produk2 -> getInfoLengkap();
+echo $produk2 -> getInfoProduk();
 
 
 
