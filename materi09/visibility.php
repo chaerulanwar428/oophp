c0921b73008fdc41ae9de9726d2780bdd5ef65d3
chaerul anwar <chaerulanwar428@gmail.com>
2022-12-07 20:26:32 +0700

    menambahkan setter dan getter

--- materi09/visibility.php
+++ materi09/visibility.php
@@ -18,6 +18,33 @@ class Produk{
 
     }
     
+    public function setJudul($judul){
+        $this->judul = $judul;
+    }
+
+    public function getJudul(){
+        return $this->judul;
+    }
+
+    public function setPenulis($penulis){
+        $this->judul = $penulis;
+    }
+
+    public function getPenulis(){
+        return $this->penulis;
+    }
+
+    public function setPenerbit(){
+        $this->penerbit = $penerbit;
+    }
+
+    public function getPenerbit(){
+        return $this->penerbit;
+    }
+
+    public function setHarga(){
+        $this->harga = $harga;
+    }
 
     public function getHarga(){
         return $this->harga - ( $this->harga * $this->diskon / 100);
